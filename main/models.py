8ac65500d4c1b5dd3ccf5e9b9cb30ff03886367a
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-02-16 15:21:47 +0500

    16.02.2024
    15:21

--- main/models.py
+++ main/models.py
@@ -8,3 +8,11 @@ class Option(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='./options/', blank=True, null=True)
 
+
+class Question(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    description = models.TextField(verbose_name='Description')
+    image = models.ImageField(upload_to='./questions/', blank=True, null=True)
+    options = models.ManyToManyField(Option, related_name='options', verbose_name='Options')
+    correct_answer = models.ManyToManyField(Option, related_name='correct_answer', verbose_name='Correct answer')
+    
